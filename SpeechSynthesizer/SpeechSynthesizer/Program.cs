9994623fcad6ff62c52c8dbdfb9ccb5c7f55ba29
Sai Deekshith <msaideekshith@gmail.com>
2020-03-28 18:25:07 +0530

    Speech synthesizer

---
+++ SpeechSynthesizer/SpeechSynthesizer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Speech.Synthesizer;
+
+namespace SpeechSynthesizer
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
